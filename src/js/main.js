6f53787f40bf204e83b1a576c8e7c42e69cf72df
lilian vignal <lilian.vignal@icloud.com>
2020-08-14 01:10:58 +0200

    email js

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,10 @@
 // systeme email
 
+$("#my-form").submit(function(e) {
+    e.preventDefault();
+  
+    var $form = $(this);
+    $.post($form.attr("action"), $form.serialize()).then(function() {
+      alert("Thank you!");
+    });
+  });
