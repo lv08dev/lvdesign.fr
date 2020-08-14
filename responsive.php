9184e94da19c6f01e4e7df743df8ae4137ac142a
lilian vignal <lilian.vignal@icloud.com>
2020-08-14 17:24:34 +0200

    Maj 1.0.1 Commandes.html

--- responsive.php
+++ responsive.php
@@ -1,3 +1,6 @@
+
+
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -21,16 +24,41 @@
             <a href="index.html" class="logo"><img src="src/img/logo v2 blanc png.png" width="75" height="75 "></a>
             <!-- MENU -->
             <ul class="menu">
-              <li><a  href="index.html">ACCEUIL</a></li>
+              <li><a  href="#">ACCEUIL</a></li>
               <li><a href="Commandes.html" style="color: #e84118;">PASSER COMMANDE</a></li>
-              <li><a href="#">A PROPROS</a></li>
+              <li><a href="A-propos.html">A PROPROS</a></li>
               <li><a href="gallery.html">GALLERIE</a></li>
             </ul>
             <a href="#" class="lang">FR</a>
           </nav>
 
 
-          
+          <div class="name">
+            <p>Bonjour</p>
+            <h1>Je m'appelle <font color="#e84118">Lilian</font> Vignal</h1>
+            <p class="details">
+              Je suis un Graphiste, Miniamaker, Monteur <font color="#e84118">Totalement Gratuit!</font> 
+               Je fait cela pour le plaisir donc je ne demande en aucun cas de l'argents,
+                pour voir mes Travaux deja realisé je t'invite a aller dans l'onglet GALLERIE ou sur mon twitter @lvdesign4 !
+            </p>
+            <!-- btn decouvrir -->
+            <a href="gallery.html" class="cv-btn">DECOUVIR</a>
+          </div>
+
+          <div class="box">
+            <h1 class="h1-box">LE MENU</h1>
+            <img class="logo-box" src="src/img/logo v1 png.png" width="65" height="65">
+            <div class="all-text-box">
+              <p class="p-box1">Je peut vous faire des <font color="#e84118">HEADERS</font> pour twitter et Youtube!</p>
+              <h3 class="h3-box1">HEADER et BANNIERE</h3>
+              <h3 class="h3-box2">LOGO et PP</h3>
+              <p class="p-box2">Je peut vous faire des <font color="#e84118">LOGO</font> pour twitter et Youtube!</p>
+              <h3 class="h3-box3">MONTAGE</h3>
+              <p class="p-box3">Je peut vous faire des montage de <font color="#e84118">Highlights Fortnite !</font></p>
+             
+            </div>
+            
+            
           </div>  
 
             <div class="social-media">
@@ -65,6 +93,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="src/js/main.js"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
   </body>
 </html>
